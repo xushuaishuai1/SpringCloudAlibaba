cc1d4b38786369643f34c3c547289e427968c002
xtm <1244688089@qq.com>
2019-11-08 13:25:33 +0800

    1

--- baseGateWay/src/main/java/com/xtm/config/RedisConfiguration.java
+++ baseGateWay/src/main/java/com/xtm/config/RedisConfiguration.java
@@ -3,7 +3,7 @@ package com.xtm.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.*;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
